12a0e30d7e049febae3f035cb912b2adce7e0d75
vijani <vijani@Acer-Aspire-5750>
2015-07-22 04:50:41 +0530

    added change passwordd tab in Settings.jsp added ChangePassword.java servlet edited settings.js

--- XCodersTimeEventManager/WebContent/settings.jsp
+++ XCodersTimeEventManager/WebContent/settings.jsp
@@ -4,6 +4,7 @@
  ----------------------------------------------------
  date		index	    name	    info
  ----------------------------------------------------
+ 20150722	13208367	vijani	added new tab to user account settings
  20150604  13208316	ravindu		created.
  ----------------------------------------------------
  --%>
@@ -23,7 +24,10 @@
 	<xf:PageHeader subtext="use this page to edit your settings" text="Settings" />
 	<xf:TabPanel>
 		<xf:TabList>
-			<xf:TabName name="Theme" active="true"/>			
+			<xf:TabName name="Theme" active="true"/>
+				<!-- (+) vijani (start) -->
+			<xf:TabName name="Account" />	
+				<!-- (+) vijani (end) -->		
 		</xf:TabList>
 	</xf:TabPanel>
 		<xf:TabBody>
@@ -40,7 +44,28 @@
 				<div class="alert alert-info" role="alert" id="settings_info"
 				style="display: none">Saving settings...</div>
 			</xf:Tab>		
-		</xf:TabBody>
+		
+		
+		<!-- (+) vijani (start) -->
+	
+			<xf:Tab name="Account" active="false">
+			<h3>Change Password</h3>
+				<form style="padding: 20px">
+					<xf:TextField id="oldpassword" label="Old Password" placeHolder="Password" type="password" name="old_password" />
+					<xf:TextField id="newpassword" label="New Password" placeHolder="Password" type="password" name="new_password" />
+					<xf:TextField id="retypenewpassword" label="Retype New Password" placeHolder="Password" type="password" name="retype_new__password" />
+					<div class="form-group">
+					<button type="button" class="btn btn-default" onclick="reset_settings()">Reset</button>
+					<button type="button" class="btn btn-primary" onclick="save_account_settings()">Save Changes</button>
+					
+					</div>
+				</form>
+				<div class="alert alert-info" role="alert" id="settings_info"
+				style="display: none">Saving settings...</div>
+			</xf:Tab>
+			</xf:TabBody>
+		<!-- (+) vijani (end) -->
+		
 	<xf:JSImports />
 	<script src="./js/settings.js"></script>
 </body>
---
+++ XCodersTimeEventManager/src/com/xcoders/servlets/ChangePassword.java
@@ -0,0 +1,100 @@
+/* 
+ *File info : Ajax servlet to change user password.
+ *File History
+ *----------------------------------------------------
+ *date		index	    name	    info
+ *----------------------------------------------------
+ *20150722  13208367	vijani    doPost method
+ *20150722  13208367	vijani		created.
+ *----------------------------------------------------
+ */
+package com.xcoders.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.xcoders.controller.EventMemberJpaController;
+import com.xcoders.model.EventMember;
+import com.xcoders.util.EncryptPassword;
+
+/**
+ * Servlet implementation class ChangePassword
+ */
+@WebServlet("/ChangePassword")
+public class ChangePassword extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ChangePassword() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// (+) 20150722 vijani (start)
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		try {
+			Boolean parametersValid = true;
+			String reply = "";
+
+			// get parameters
+			String oldPassword = request.getParameter("oldPassword");
+			String newPassword = request.getParameter("newPassword");
+			String retypeNewPassword = request
+					.getParameter("retypeNewPassword");
+
+			// validate
+			if (!newPassword.equals(retypeNewPassword)) {
+				reply = "New passwords does not match!";
+			}
+
+			// action
+			if (parametersValid) {
+				String encryptedOldPassword;
+				encryptedOldPassword = EncryptPassword.hashPassword(
+						oldPassword, "salt");
+
+				String user = request.getRemoteUser();
+				if (user == null) {
+					reply = "Please login!";
+				} else {
+					System.out.println(">>> USer : " + user);
+					EventMember em = new EventMemberJpaController()
+							.findEventMemberByUserName(user);
+					String userPassword = em.getPassword();
+					if (!encryptedOldPassword.equals(userPassword)) {
+						reply = "Your old password is incorrect, please login with correct password";
+						// call sign out here 
+					}else{
+						em.setPassword(newPassword);
+						reply = "s";
+						//reply = "Your password has changed. Please sign up with your new password!"; 
+						// call sign out here 
+					}
+				}
+			}
+
+		} catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+			e.printStackTrace();
+		}
+
+		// (+) 20150722 vijani (end)
+	}
+
+}
